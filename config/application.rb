f88b8363ceddcdc28587be2a557a5d8cf98f4deb
NicolasSebille <nsebille.pro@gmail.com>
2020-04-30 20:56:32 +0200

    assets issue

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module TestAp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 	# config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-
+	config.assets.enabled = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
