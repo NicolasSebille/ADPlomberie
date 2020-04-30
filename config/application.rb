35d1a2494683aa6f8acdc22a6e0603baa49aaa93
NicolasSebille <nsebille.pro@gmail.com>
2020-04-30 19:33:32 +0200

    fix heroku

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
 module TestAp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+	config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
